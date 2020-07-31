de4551c98401985d69a5b4a3dfd0ffa197c29184
MarioEberth <marioeberth@gmail.com>
2020-07-31 22:04:12 +0200

    update buttomNavigationBar

--- Fragments/Fragment/app/src/main/java/com/example/fragment/MainActivity.kt
+++ Fragments/Fragment/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.fragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -13,6 +15,8 @@ class MainActivity : AppCompatActivity() {
 
         val buttomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.profileFragment, R.id.settingsFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
         buttomNavigationView.setupWithNavController(navController)
     }
