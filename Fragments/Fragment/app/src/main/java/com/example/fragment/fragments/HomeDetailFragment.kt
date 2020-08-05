abe9d844328c006d78659e5940abc542cc53797f
MarioEberth <marioeberth@gmail.com>
2020-08-05 14:28:28 +0200

    update home screen with tabLayout and overview and detail fragment

--- Fragments/Fragment/app/src/main/java/com/example/fragment/fragments/HomeDetailFragment.kt
+++ Fragments/Fragment/app/src/main/java/com/example/fragment/fragments/HomeDetailFragment.kt
@@ -5,16 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.widget.ViewPager2
 import com.example.fragment.R
-import com.example.fragment.onboarding.ViewPagerAdapter
-import com.example.fragment.onboarding.screens.FirstScreen
-import com.example.fragment.onboarding.screens.SecondScreen
-import com.example.fragment.onboarding.screens.ThirdScreen
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
-import kotlinx.android.synthetic.main.fragment_home.view.*
-import kotlinx.android.synthetic.main.fragment_view_pager.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [HomeDetailFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class HomeDetailFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -44,21 +35,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-        val fragmentlist = arrayListOf<Fragment>(
-            HomeOverviewFragment(),
-            HomeDetailFragment()
-        )
-        val adapter = ViewPagerAdapter(fragmentlist,requireActivity().supportFragmentManager, lifecycle)
-        view.viewPagerHome.adapter = adapter
-
-        //tabLayout
-        var tabLayout: TabLayout = view.findViewById(R.id.tabLayout)
-        var names: Array<String> = arrayOf("Overview","Detail")
-        TabLayoutMediator(tabLayout,view.viewPagerHome){tab, position ->
-            tab.text = names[position]
-        }.attach()
-        return view
+        return inflater.inflate(R.layout.fragment_home_detail, container, false)
     }
 
     companion object {
@@ -68,12 +45,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment firstFragment.
+         * @return A new instance of fragment HomeDetailFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            HomeDetailFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
